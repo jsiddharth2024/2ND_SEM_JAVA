5593207819e7d585287159c63ba29ffcd5f36e44
jsiddharth2024 <125435918+jsiddharth2024@users.noreply.github.com>
2023-06-07 15:21:23 +0530

    Add files via upload

---
+++ LC2/String_man.java
@@ -0,0 +1,20 @@
+//String Manipulation
+import java.util.Scanner;
+public class String_man{
+    public static void main(String[] args) {
+        System.out.println("J SIDDHARTH");
+        System.out.println("SJC22MCA-2034");
+        System.out.println("27-04-2023");
+        System.out.println("20MCA132 , Object Oriented Programming Lab \n"); 
+        System.out.println("Enter The String");
+        Scanner sc = new Scanner(System.in);
+        String str1 = sc.nextLine();
+        System.out.println("Length of String = "+str1.length());
+        System.out.println("Character at First position  = "+str1.charAt(1));
+        System.out.println("String Contains 'Col' sequence :"+str1.contains("Col"));
+        System.out.println("String ends with e : "+str1.endsWith("e"));
+        System.out.println("Replace'col' with 'kol' : "+str1.replaceAll("Col","kol"));
+        System.out.println("LOWERCASE : "+str1.toLowerCase());
+        System.out.println("UPPERCASE : "+str1.toUpperCase());
+}
+}
